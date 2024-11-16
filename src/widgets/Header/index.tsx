fba13ff8066c2e7b72c3f2c89e23e2b9ff4ee17b
Parle648 <sashahoumi@gmail.com>
2024-11-16 18:03:08 +0200

    + Create: dark mode feature

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react"
 import { Link } from "react-router-dom"
 import "./styles.css"
+import { useDarkMode } from "../../app/contexts/dark-mode-context"
 
 const Header = () => {
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode")
+    } else {
+      document.body.classList.remove("dark-mode")
+    }
+  }, [isDarkMode])
+
   return (
-    <header>
+    <header
+      style={{
+        display: "flex",
+        width: "100vw",
+        padding: "20px 150px",
+      }}
+    >
       <nav>
         <ul>
           <li>
@@ -14,6 +32,7 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </header>
   )
 }
