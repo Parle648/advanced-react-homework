225306499592b546acf30fbe005d3d005faf7a1b
Parle648 <sashahoumi@gmail.com>
2024-11-16 17:27:07 +0200

    * Update: UserDetailsPage content

--- src/pages/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage.tsx
@@ -1,5 +1,25 @@
+import { useParams } from "react-router-dom"
+import { users } from "../shared/constants/users-mock-data"
+import { IUser } from "../shared/types/interfaces"
+import UserCard from "../entities/UserCard"
+
 const UserDetailsPage = () => {
-  return <div>UserDetailsPage</div>
+  const { id } = useParams()
+  const currentUser: IUser = users.find((user) => `${user.id}` === id) as IUser
+
+  return (
+    <div>
+      {
+        <UserCard
+          id={currentUser.id}
+          email={currentUser.email}
+          first_name={currentUser.first_name}
+          last_name={currentUser.last_name}
+          avatar={currentUser.avatar}
+        />
+      }
+    </div>
+  )
 }
 
 export default UserDetailsPage
