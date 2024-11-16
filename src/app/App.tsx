e7e658ecd58aed8aad6e548c795a5139a60313fb
Parle648 <sashahoumi@gmail.com>
2024-11-16 17:14:15 +0200

    + Create: routes layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,23 @@
 import UsersPage from "../pages/UsersPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { appStateStore } from "./store"
+import UserDetailsPage from "../pages/UserDetailsPage"
+import Home from "../pages/Home"
 
 function App() {
-  return <UsersPage />
+  return (
+    <Provider store={appStateStore}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/users" element={<UsersPage />}></Route>
+          <Route path="/users/:id" element={<UserDetailsPage />}></Route>
+          <Route path="*" element={"404 Not found page"} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 export default App
